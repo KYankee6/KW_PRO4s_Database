16fd367511a287a3eca82671bffd5bc2d7fb0f56
ChickenRushKR <zx8635@gmail.com>
2020-11-13 14:09:38 +0900

    Add stydent_sys.js and main.ejs

--- practice1/app.js
+++ practice1/app.js
@@ -4,26 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var joinRouter = require('./routes/joinForm');
+//var join = require('./routes/joinForm');
+//var board = require('./routes/board');
 var app = express();
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//app.use('/board/read/', express.static('./image'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/join', joinRouter);
-
+//app.use('/join', join);
+//app.use('/board', board);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
