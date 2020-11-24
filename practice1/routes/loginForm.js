9765606b3668d804eba966f0f0b7d3bf9bb47d8c
KYankee6 <highestbright98@naver.com>
2020-11-24 20:59:23 +0900

    login 수정

--- practice1/routes/loginForm.js
+++ practice1/routes/loginForm.js
@@ -1,23 +1,35 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
+var serverStatic = require('serve-static');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
+//var passport = require('./config/passport');
 var bodyParser = require('body-parser');
-
 var mysql = require('mysql');
+var crypto = require('crypto');
+var url = require('url');
+var bodyParser_post = require('body-parser');
+
+
+//app.use(serveStatic(path.join(__dirname, 'public')));
+
+
 var connection = mysql.createConnection({
   connectionLimit: 5,
-    host: '223.194.46.205',
-    port:3306,
-    database: 'database2',
-    user: 'root',
-    password: 'pro4spro4s!'
+  host: '223.194.46.205',
+  port: 3306,
+  database: 'database2',
+  user: 'root',
+  password: 'pro4spro4s!'
 });
-connection.connect((err)=>{
-  if(err){
+connection.connect((err) => {
+  if (err) {
     console.log('Not connected to DB');
     throw err;
   }
-  else{
+  else {
     console.log('Sucess Connected');
   }
 });
@@ -25,46 +37,92 @@ const { request, response } = require('../app');
 
 
 var app = express();
-
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
-  secret:'secret',
+  secret: 'secret',
   resave: true,
   saveUninitialized: true
 }));
+var router = express.Router();
+
+// app.use(passport.initialize());
+// app.user(passport.session());
+// app.use(function(req,res,next){
+//   res.locals.isAuthenticated = req.isAuthenticated();
+//   res.locals.currentUser = req.user;
+//   next();
+// });
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('loginForm',{title: 'login'});
+app.get('/', function (req, res, next) {
+  console.log('세션 유저 확인후 루팅');
+  if (req.session.user) {
+    res.redirect('/index', { title: 'login' });
+  }
+  else {
+    res.render('loginForm');
+  }
 });
 
-router.post('/', function(req,res,next){
-  var stu_id=req.body.student_id;
-  var passwd = req.body.password;
-  if(stu_id && passwd){
+
+
+app.post('/', function (req, res, next) {
+  var in_stu_id = req.body.student_id;
+  var in_passwd = req.body.password;
+  var salt = Math.round((new Date().valueOf() + Math.random())) + "";
+  var hashPassword = crypto.createHash("sha512").update(in_passwd + salt).digest("hex");
+
+  // var reuslt = models.user.create({
+  //   name:in_stu_id,
+  //   passwd: hashPassword,
+  //   salt: salt
+  // })
+  if (req.session.user) {
+    console.log('already logged in');
+    res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+    res.write('<h1>already Login</h1>');
+    res.write('<a href="/index">Move</a>');
+    res.end();
+  }
+  else if (in_stu_id && in_passwd) {
     var sqlForIDPW = "select * from register_info where ID =? and Passwd = ?";
-    connection.query(sqlForIDPW, [stu_id,passwd], function(error,results,fields){
-      if(error){
-        console.error("err: "+error);
-          console.log(results);
-          console.log(fields);
+    connection.query(sqlForIDPW, [in_stu_id, in_passwd], function (error, results, fields) {
+      if (error) {
+        console.error("err: " + error);
+        console.log(results);
+        console.log(fields);
       }
-      else if(results.length>0){
+      else if (results.length > 0) {
         //req.session.loggedin=true;
         //req.session.stu_id = student_id;
-        res.redirect('/');
+        // res.json({stu_id :in_stu_id});
+        req.session.user =
+        {
+          id: in_stu_id,
+          pw: in_passwd,
+          name: 'UsersNames!!!!!',
+          authorized: true
+        };
+        res.redirect(url.format({
+          pathname: "/",
+          query: {
+            "stu_id": req.body.student_id
+          }
+        }));
       }
-      else{
-        res.redirect('/login?e=' + encodeURIComponent('Incorrect username or password'));
+      else {
+        res.send("<script>alert('아이디 혹은 비밀번호를 확인하세요.');history.back();</script>")
+        //res.redirect('/login?e=' + encodeURIComponent('Incorrect username or password'));
       }
       res.end();
     });
-  }else{
+  } else {
     res.send('아이디 그리고 비밀번호를 입력해주세요');
     res.end();
   }
 });
 
-module.exports = router;
+module.exports = app;
